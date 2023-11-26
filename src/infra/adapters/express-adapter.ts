8c629d4456d30209535874b6ad56c4102eb19c3f
Jairo Chaves <jairochaves2@gmail.com>
2023-11-26 18:21:58 -0300

    refact: change express application

--- src/infra/adapters/express-adapter.ts
+++ src/infra/adapters/express-adapter.ts
@@ -1,10 +1,15 @@
-import Express from "express";
+import express from "express";
 import { Server } from "http";
 import HttpServer from "../../domain/interfaces/http-server";
 
 export default class ExpressAdapter implements HttpServer {
+  private app: express.Application;
+
+  constructor() {
+    this.app = express();
+  }
   start(port: number): Server {
-    return Express().listen(port);
+    return this.app.listen(port);
   }
   stop(server: Server): void {
     server.close();
